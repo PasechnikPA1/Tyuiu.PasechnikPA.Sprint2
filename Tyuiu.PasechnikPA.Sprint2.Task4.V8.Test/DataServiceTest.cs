9a37d5dd6f90d9edeb28ca42bdb2bc5dda3938a4
Pavel Pasechnik <Tyuiu.PasechnikPA@outlook.com>
2023-10-11 22:22:09 +0500

    task4

--- Tyuiu.PasechnikPA.Sprint2.Task4.V8.Test/DataServiceTest.cs
+++ Tyuiu.PasechnikPA.Sprint2.Task4.V8.Test/DataServiceTest.cs
@@ -1,14 +1,33 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
+
+using Tyuiu.PasechnikPA.Sprint2.Task4.V8.Lib;
+
 namespace Tyuiu.PasechnikPA.Sprint2.Task4.V8.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCondition1()
         {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 9824377.207;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = -6;
+            Assert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.PasechnikPA.Sprint2.Task4.V8/Program.cs
+++ Tyuiu.PasechnikPA.Sprint2.Task4.V8/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.PasechnikPA.Sprint2.Task4.V8.Lib;
+
 namespace Tyuiu.PasechnikPA.Sprint2.Task4.V8
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Пасечник П. А. | АСОиУБ-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся струры                                  *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Варинант #8                                                            *");
+            Console.WriteLine("* Выполнил: Пасечник Павел Андреевич | АСОиУБ-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*Написать программу, которая вычисляет требуемое значение с использованием*");
+            Console.WriteLine("*тернарного оператора, где пользователь вводит значение переменных x,y с **");
+            Console.WriteLine("*клавиатуры если x * 2 > y + 15, то z = () иначе ()************************");
+            Console.WriteLine("*трех знаков после запятой.************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            Console.WriteLine("Введите значение переменной Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x,y);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
         }
     }
 }
